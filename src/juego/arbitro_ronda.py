6975715c59c501ab18e23aa9cc53e684d1d1a8de
Diego-ipt <diperez2022@udec.cl>
2025-09-02 13:26:05 -0400

    GREEN: implementación mínima para arbitro ronda

---
+++ src/juego/arbitro_ronda.py
@@ -0,0 +1,29 @@
+from src.juego.contador_pintas import ContadorPintas
+
+
+class ArbitroRonda:
+    def resolver_duda(self, cachos, jugada):
+        _, pinta, cantidad = jugada
+        contador = ContadorPintas()
+        total = contador.contar(cachos, pinta)
+        if total >= cantidad:
+            return "pierde_dudador"
+        else:
+            return "pierde_apostador"
+
+    def resolver_calzar(self, cachos, jugada):
+        jugador_idx, pinta, cantidad = jugada
+        contador = ContadorPintas()
+        total = contador.contar(cachos, pinta)
+        dados_en_juego = sum(len(c.get_pintas_de_dados() or []) for c in cachos)
+        total_dados = dados_en_juego
+        jugador_dados = len(cachos[jugador_idx].get_pintas_de_dados() or [])
+        if not self.puede_calzar(total_dados, dados_en_juego, jugador_dados):
+            return "no_se_puede_calzar"
+        if total == cantidad:
+            return "gana_calzador"
+        else:
+            return "pierde_calzador"
+
+    def puede_calzar(self, total_dados, dados_en_juego, jugador_dados):
+        return dados_en_juego >= total_dados / 2 or jugador_dados == 1
