ca00f1b5f7a6face402b0d9fc942227a2e9de9cc
Diego-ipt <diperez2022@udec.cl>
2025-09-02 13:45:19 -0400

    REFACTOR: mejora código de arbitro ronda

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -2,10 +2,16 @@ from src.juego.contador_pintas import ContadorPintas
 
 
 class ArbitroRonda:
+    def __init__(self):
+        self.contador = ContadorPintas()
+        self.usar_ases_comodin = True  # Por defecto, ases son comodines
+
+    def set_ases_comodin(self, valor: bool):
+        self.usar_ases_comodin = valor
+
     def resolver_duda(self, cachos, jugada):
         _, pinta, cantidad = jugada
-        contador = ContadorPintas()
-        total = contador.contar(cachos, pinta)
+        total = self.contador.contar(cachos, pinta, ases_comodin=self.usar_ases_comodin)
         if total >= cantidad:
             return "pierde_dudador"
         else:
@@ -13,8 +19,7 @@ class ArbitroRonda:
 
     def resolver_calzar(self, cachos, jugada):
         jugador_idx, pinta, cantidad = jugada
-        contador = ContadorPintas()
-        total = contador.contar(cachos, pinta)
+        total = self.contador.contar(cachos, pinta, ases_comodin=self.usar_ases_comodin)
         dados_en_juego = sum(len(c.get_pintas_de_dados() or []) for c in cachos)
         total_dados = dados_en_juego
         jugador_dados = len(cachos[jugador_idx].get_pintas_de_dados() or [])
